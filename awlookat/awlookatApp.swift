15fde723316495674200636b3c8c58ed5c791027
John Endres <endres@macplugins.com>
2021-11-05 15:12:40 -0400

    Initial Commit

---
+++ awlookat/awlookatApp.swift
@@ -0,0 +1,17 @@
+//
+//  awlookatApp.swift
+//  awlookat
+//
+//  Created by John Endres on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct awlookatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
